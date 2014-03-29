1ee656b2d5fccf7685e96c8a5469ac1b748feb71
Jordan Leigh <AlwaysBCoding@gmail.com>
2014-03-29 12:37:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homeauto::Application.config.secret_key_base = 'b5e9abb5b8a588f72a16dd9494810c46c2baaeef93941f70eeba7679ebe3d422ff209585ac1f773e6652e56e8a113677c5650b7bfd0d7dd69a8a536b6785ebf4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Homeauto::Application.config.session_store :cookie_store, key: '_homeauto_session'
